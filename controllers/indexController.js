4c85cc7516c3017ef4508668a37010ebf10b5dc8
kupathebest <kupa.thebest@gmail.com>
2022-07-22 12:42:12 -0300

    avanzando estructura

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,8 @@
 module.exports = {
     index: (req,res) => {
         return res.render('index')
+    },
+    prueba: (req, res) => {
+        return res.send(req.body)
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let {index} = require('../controllers/indexController')
+let {index, prueba} = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', index);
+router.post('/', prueba)
 
 module.exports = router;
